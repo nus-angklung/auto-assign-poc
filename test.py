41e78444956f2ab55c56f1fd9367901b6b177f04
Bennett Clement <ben@benclmnt.com>
2021-05-16 20:55:29 +0800

    Add some parsing methods

--- test.py
+++
@@ -1,7 +0,0 @@
-from docx2python.iterators import docx2python
-from docx2python.iterators import enum_at_depth
-
-f = docx2python('Avengers.docx')
-
-for i in range(len(f[1])):
-    print(f.body[1][i])
